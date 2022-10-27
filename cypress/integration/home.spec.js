52ad8de3b91756813c4a2447ac5f9079dd32c44f
zhuicode99 <enlin291814833@hotmail.com>
2022-10-27 01:31:24 +0000

    add cypress for home-spec

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('example to-do app', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
+})
